00e04461674c2325cf5fb6c20ff4efd7b87fb37c
shane18l <lmlfreakspam@gmail.com>
2025-09-10 15:42:13 -0400

    changed file names and working on testing the model

---
+++ create_dataset.py
@@ -0,0 +1,62 @@
+import os
+import pandas as pd
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+folder_name = 'tone_perfect'
+csv_file_path = 'mandarin_tone_dataset2.csv'
+
+
+X, Y = [], []
+
+
+
+def extract_f0(file_path):
+    y, sr = librosa.load(f"tone_perfect/{file_path}")
+    f0, voiced_flag, voiced_probs = librosa.pyin(y, 
+                                                     sr=sr,
+                                                     fmin=75,
+                                                     fmax=500)
+    f0 = np.nan_to_num(f0)
+    nonzero_idx = np.where(f0 != 0)[0]
+
+    if len(nonzero_idx) == 0:
+        return 
+
+    f0_non_silent = f0[nonzero_idx[0]:nonzero_idx[-1] + 1]
+    f0_non_silent = (f0_non_silent - np.mean(f0_non_silent)) / (np.std(f0_non_silent) + 1e-6)
+
+
+
+
+    f0_interp = np.interp(
+        np.linspace(0, len(f0_non_silent)-1, 50),
+        np.arange(len(f0_non_silent)), 
+        f0_non_silent
+    )
+    
+    
+
+   
+    match = re.search(r'\d+', file_path)
+    if match: 
+        tone_num = int(match.group())
+    else:
+        tone_num = 0
+
+
+    X.append(f0_interp)
+    Y.append(tone_num)
+
+
+for i, file in enumerate(os.listdir(folder_name)):
+    extract_f0(file)
+
+  
+df = pd.DataFrame(X)
+df["tone"] = Y
+df.to_csv("mandarin_tone_sequences.csv", index=False)
